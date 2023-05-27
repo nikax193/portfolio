63d7b421f9703e7e8bcab64c09c7d2607ffaf5f0
Nika <nikax193@gmail.com>
2023-05-28 00:55:24 +0400

    add section skills, add style skills.scss, add script for count persent

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,9 @@ hamburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__ratings-counter'),
+                    lines = document.querySelectorAll('.skills__ratings-line span');
+counters.forEach((item, i)=> {
+    lines[i].style.width = item.innerHTML
+})
